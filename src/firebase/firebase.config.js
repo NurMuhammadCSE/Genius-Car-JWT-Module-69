29b1f568d0736d30215d20acbfc8ba32e42b049e
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-02-21 18:00:11 +0600

    mongodb-crud

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,40 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_apiKey,
+//   authDomain: process.env.REACT_APP_authDomain,
+//   projectId: process.env.REACT_APP_projectId,
+//   storageBucket: process.env.REACT_APP_storageBucket,
+//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   appId: process.env.REACT_APP_appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1CgzgUqLq_7r-yTAblFesHzC0qbLNt7I",
+  authDomain: "genius-car-91d12.firebaseapp.com",
+  projectId: "genius-car-91d12",
+  storageBucket: "genius-car-91d12.appspot.com",
+  messagingSenderId: "13943919859",
+  appId: "1:13943919859:web:72fd2dff16e0ecfeafd386"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
